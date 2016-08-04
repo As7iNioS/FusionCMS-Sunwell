f25e2e8a557c143a1dccc3fcac34f2e7a9b53353
Lukasz Kolasa <maczugapl@gmail.com>
2016-08-04 23:25:33 +0200

    Some cleaning / warning fixes by PHPStorm.

--- application/third_party/MX/Controller.php
+++ application/third_party/MX/Controller.php
@@ -38,99 +38,90 @@ require dirname(__FILE__).'/Base.php';
  **/
 class MX_Controller 
 {
-	public $autoload = array();
-	private $standardModule = "news";
+    public $autoload = array();
+    private $standardModule = "news";
 
-	public function __construct()
-	{
-		$class = str_replace(CI::$APP->config->item('controller_suffix'), '', get_class($this));
-		
-		log_message('debug', $class." MX_Controller Initialized");
+    public function __construct()
+    {
+        $class = str_replace(CI::$APP->config->item('controller_suffix'), '', get_class($this));
 
-		Modules::$registry[strtolower($class)] = $this;
+        log_message('debug', $class . " MX_Controller Initialized");
 
-		// We need the MODULE name, not the controller
-		$moduleName = CI::$APP->uri->segment(1);
+        Modules::$registry[strtolower($class)] = $this;
 
-		// Default module
-		if(!$moduleName)
-		{
-			$moduleName = $this->standardModule;
-		}
+        // We need the MODULE name, not the controller
+        $moduleName = CI::$APP->uri->segment(1);
 
-		// Check if the path exists to the requested module, also make sure to check if the module exists.
-		$path = APPPATH.'modules/'.strtolower($moduleName);
+        // Default module
+        if (!$moduleName) {
+            $moduleName = $this->standardModule;
+        }
 
-		// Does the folder exist?
-		if(!is_dir($path))
-		{
-			CI::$APP->template->show404();
-		}
+        // Check if the path exists to the requested module, also make sure to check if the module exists.
+        $path = APPPATH . 'modules/' . strtolower($moduleName);
 
-		// Make sure the module has a manifest
-		if(!file_exists($path."/manifest.json"))
-		{
-			show_error("The manifest.json file for <b>".strtolower($moduleName)."</b> does not exist");
-		}
+        // Does the folder exist?
+        if (!is_dir($path)) {
+            CI::$APP->template->show404();
+        }
 
-		$module = file_get_contents($path."/manifest.json");
-		$module = json_decode($module, true);
+        // Make sure the module has a manifest
+        if (!file_exists($path . "/manifest.json")) {
+            show_error("The manifest.json file for <b>" . strtolower($moduleName) . "</b> does not exist");
+        }
 
-		// Make sure it was real JSON
-		if(!is_array($module))
-		{
-			show_error("The manifest.json file for <b>".strtolower($moduleName)."</b> is not properly formatted");
-		}
+        $module = file_get_contents($path . "/manifest.json");
+        $module = json_decode($module, true);
 
-		// Is the module enabled?
-		if(!isset($module['enabled']) || !$module['enabled'])
-		{
+        // Make sure it was real JSON
+        if (!is_array($module)) {
+            show_error("The manifest.json file for <b>" . strtolower($moduleName) . "</b> is not properly formatted");
+        }
+
+        // Is the module enabled?
+        if (!isset($module['enabled']) || !$module['enabled']) {
             redirect('cms_error');
 //			CI::$APP->template->show404();
-		}
+        }
 
-		// Default to current version
-		if(!array_key_exists("min_required_version", $module))
-		{
-			$module['min_required_version'] = CI::$APP->config->item('FusionCMSVersion');
-		}
+        // Default to current version
+        if (!array_key_exists("min_required_version", $module)) {
+            $module['min_required_version'] = CI::$APP->config->item('FusionCMSVersion');
+        }
 
-		// Does the module got the correct version?
-		if(!CI::$APP->template->compareVersions($module['min_required_version'], CI::$APP->config->item('FusionCMSVersion')))
-		{
-			show_error("The module <b>".strtolower($moduleName)."</b> requires FusionCMS v".$module['min_required_version'].", please update at fusion-hub.com");
-		}
+        // Does the module got the correct version?
+        if (!CI::$APP->template->compareVersions($module['min_required_version'], CI::$APP->config->item('FusionCMSVersion'))) {
+            show_error("The module <b>" . strtolower($moduleName) . "</b> requires FusionCMS v" . $module['min_required_version'] . ", please update at fusion-hub.com");
+        }
 
-		/* copy a loader instance and initialize */
-		$this->load = clone load_class('Loader');
-		$this->load->initialize($this);	
-		
-		/* autoload module items */
-		$this->load->_autoloader($this->autoload);
+        /* copy a loader instance and initialize */
+        $this->load = clone load_class('Loader');
+        $this->load->initialize($this);
 
-		$this->cookieLogIn();
-	}
-	
-	public function __get($class) {
-		return CI::$APP->$class;
-	}
+        /* autoload module items */
+        $this->load->_autoloader($this->autoload);
 
-	public function cookieLogIn()
-	{
-		if(!CI::$APP->user->isOnline())
-		{
-			$username = CI::$APP->input->cookie("fcms_username");
-			$password = CI::$APP->input->cookie("fcms_password");
+        $this->cookieLogIn();
+    }
 
-			if($username && $password)
-			{
-				$check = CI::$APP->user->setUserDetails($username, $password);
+    public function cookieLogIn()
+    {
+        if (!CI::$APP->user->isOnline()) {
+            $username = CI::$APP->input->cookie("fcms_username");
+            $password = CI::$APP->input->cookie("fcms_password");
 
-				if($check == 0)
-				{
-					redirect('news');
-				}
-			}
-		}
-	}
+            if ($username && $password) {
+                $check = CI::$APP->user->setUserDetails($username, $password);
+
+                if ($check == 0) {
+                    redirect('news');
+                }
+            }
+        }
+    }
+
+    public function __get($class)
+    {
+        return CI::$APP->$class;
+    }
 }
